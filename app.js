8c45d2cd6196775cf4c91838cc990621ae40ab93
Student <Student>
2017-09-20 12:12:49 +0300

    my work

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 let player = new AudioPlayer();
 
 function renderItems(data) {
+	const ul = document.querySelector('ul.playList');
+	ul.innerHTML = '';
+	data.forEach( function(item) {
+		const li = document.createElement('li');
+		li.textContent = item.trackName;
+
+		li.onclick = (e) => {
+			player.play(item);
+		}
+
+		ul.appendChild(li);
+	})
     // render items to the browser
 }
 
@@ -9,10 +21,28 @@ const searchForm = document.querySelector('form');
 
 searchForm.onsubmit = (e) => {
 
+	console.log('my form was submitted')
+
     // prevent normal form submission
     e.preventDefault();
 
     // fetch items here
 
-    let endpoint = 'https://itunes.apple.com/search?term=bob+dylan';
+    
+    let artistName = document.querySelector('#artist').value;
+
+    let endpoint = 'https://itunes.apple.com/search?term=' + artistName;
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('load', (e) =>{
+    	let response = JSON.parse(xhr.responseText);
+    	renderItems(response.results);
+
+    	//console.log('finished');
+    });
+
+    xhr.open('Get', endpoint, true);
+
+    xhr.send();
 }
